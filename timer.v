2abf8025734d0d9b089e4c6b6a7b20db1394dd42
Justine <justinejacobson@u.boisestate.edu>
2026-04-27 18:40:58 -0600

    Update timer.v

--- timer.v
+++ timer.v
@@ -2,17 +2,17 @@
 module timer(
     input clk,
     input rst,
-    input en,
-    input load,
+    input en,  // Enable/Disable clk
+    input load, //load = 1, load counter with "load_value"
     input [5:0] load_value,     // value to load into counter
-    output reg [5:0] state      // 6-bit register (D flip-flops)
+    output reg [5:0] state      // 6-bit register to represent highest number (59)
 );
     // This always block represents sequential logic (flip-flops)
     always @(posedge clk or posedge rst) begin
         if (rst)
             state <= 6'd0;              // reset to 0
         else if (load)
-            state <= load_value;        // synchronous load: set counter to load_value
+            state <= load_value;        // synchronous load: set state =  load_value
         else if (en) begin
             if (state == 6'd0)
                 state <= 6'd0;          // hold at 0 (no wrap-around)
